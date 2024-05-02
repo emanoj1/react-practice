c08c2700502a692df2e0b027b6def86a7654251e
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2024-05-02 18:31:55 +1000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+// const welcome = {
+//    greeting: 'Hey',
+//    title: 'React'
+//  }
+
+function getTitle(title) {
+  return title;
+}
+
+function App() {
+  return (
+    <div>
+      
+      <h1>Hello {getTitle('React')}</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
+
+export default App;
