3f9580a79ec7b0d546299beb90ec35f7953abcb6
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2024-05-10 17:23:29 +1000

    Practice arrows, Handler function

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
 
-// const welcome = {
-//    greeting: 'Hey',
-//    title: 'React'
-//  }
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+},
+{
+  title: 'Redux',
+  url: 'https://redux.js.org/',
+  author: 'Dan, Andrew',
+  num_comments: 2,
+  points: 5,
+  objectID:1,
+},
+];
 
-function getTitle(title) {
-  return title;
-}
+const App = () => (
+    <div>
+      <h1>My Hacker Stories</h1>
 
-function App() {
+      <Search />
+
+      <hr />
+
+      <List />
+    </div>
+  );
+
+const Search = () => {
+  const handleChange = (event) => {
+    console.log(event);
+    console.log(event.target.value);
+  }
   return (
     <div>
-      
-      <h1>Hello {getTitle('React')}</h1>
-
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <label htmlFor = "search">Search: </label>
+      <input id = "search" type="text" onChange={handleChange}/>
     </div>
   );
 }
+
+const List = () => (
+    <ul>
+      {list.map((item) => (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+      ))}
+    </ul>
+);
+
+
+// // const welcome = {
+// //   greeting: 'Hey',
+// //   title: 'Manoj!'
+// // }
+
+// function getTitle(title){
+//   return title;
+// }
+
+// function App(){
+//   return (
+//     <div>
+//       {/* <h1>{welcome.greeting} {welcome.title}</h1> */}
+//       <h1>Hello, {getTitle('Manoj!')}</h1>
+
+//       <label htmlFor='search'>Search: </label>
+//       <input id="search" type="text"/>
+
+//     </div>
+//   );
+// }
 
 export default App;
